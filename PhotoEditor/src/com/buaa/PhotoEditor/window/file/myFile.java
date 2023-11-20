d2e05cc50611c48fa604db418c708fe665357adc
Lyx-Roisy <2059218150@qq.com>
2023-11-20 12:45:05 +0800

    编写myFile（原File）与open
    本地等价替换测试完成。
    注意，目录层级有修改，故window类中目录层级部分代码会有相应变化

---
+++ PhotoEditor/src/com/buaa/PhotoEditor/window/file/myFile.java
@@ -0,0 +1,31 @@
+package com.buaa.PhotoEditor.window.file;
+
+import com.buaa.PhotoEditor.window.Window;
+
+import javax.swing.*;
+
+public class myFile {
+    public JMenu fileMenu;
+    private Window window;
+    private Open open;
+    private Save save;
+
+    public myFile(Window window) {
+        this.window = window;
+        this.open = new Open(window);
+        this.save = new Save(window);
+        fileMenu = new JMenu("File");
+    }
+
+    public Window getWindow() {
+        return window;
+    }
+
+    public Open getOpen() {
+        return open;
+    }
+
+    public Save getSave() {
+        return save;
+    }
+}
